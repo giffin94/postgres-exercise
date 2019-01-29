ed6af1d74d5155a59aaeb63cf63a4ac48ab5e4b3
Aedan <aedan.giffin@gmail.com>
2019-01-29 19:56:59 +0000

    Add functionality to take command from user and return formatted results matching their name-related query

--- test_script.js
+++ test_script.js
@@ -1,25 +1,38 @@
 const pg = require("pg");
 const settings = require("./settings");
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-});
+const data = process.argv.slice(2)[0];
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);;
-  }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+const client = new pg.Client(settings);
+
+client.connect();
+
+function doQuery(client, query, values) {
+  client.query(query, values, (err, res) => {
     if (err) {
-      return console.error("error running query", err);
+      console.log(err);
+      return false;
+    } else {
+      console.log("Searching ...");
     }
-    console.log(result);
-    console.log(result.rows[0].number);
+
+    if (res.rows.length) {
+      console.log(`Found ${res.rows.length} for name ${data}.`);
+      let c = 1;
+      res.rows.forEach((famous_people) => {
+        console.log(`${c++}- ${famous_people.first_name} ${famous_people.last_name}, born '${famous_people.dob}'`)
+      });
+    } else {
+      console.log("No results found");
+    }
     client.end();
   });
-});
+}
+
+if (data) {
+  const query = "SELECT first_name, last_name, to_char(birthdate, 'YYYY-MM-DD') AS dob FROM famous_people WHERE first_name LIKE $1 OR last_name LIKE $1";
+  const values = [`%${data}%`];
+  doQuery(client, query, values);
+}
+
+
